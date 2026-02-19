c8dfc90a865b0703e5c3123cd058a3579b99f549
Antonio Membrides Espinosa <tonykssa@gmail.com>
2026-02-19 11:23:14 +0100

    v1.0.7 (#16)
    * update docs
    
    * Fixing dependency vulnerabilities
    
    * migrate to kozen namespace
    
    * v1.0.5
    
    * v1.0.6
    
    * v1.0.7

--- src/models/TriggerDelegate.ts
+++ src/models/TriggerDelegate.ts
@@ -7,5 +7,17 @@ export interface ITriggerDelegate {
     insert?: (change: ChangeStreamDocument<Document>, tools?: ITriggerTools) => void;
     update?: (change: ChangeStreamDocument<Document>, tools?: ITriggerTools) => void;
     delete?: (change: ChangeStreamDocument<Document>, tools?: ITriggerTools) => void;
+    remove?: (change: ChangeStreamDocument<Document>, tools?: ITriggerTools) => void;
     replace?: (change: ChangeStreamDocument<Document>, tools?: ITriggerTools) => void;
+    drop?: (change: ChangeStreamDocument<Document>, tools?: ITriggerTools) => void;
+    rename?: (change: ChangeStreamDocument<Document>, tools?: ITriggerTools) => void;
+    dropDatabase?: (change: ChangeStreamDocument<Document>, tools?: ITriggerTools) => void;
+    invalidate?: (change: ChangeStreamDocument<Document>, tools?: ITriggerTools) => void;
+    createIndexes?: (change: ChangeStreamDocument<Document>, tools?: ITriggerTools) => void;
+    create?: (change: ChangeStreamDocument<Document>, tools?: ITriggerTools) => void;
+    modify?: (change: ChangeStreamDocument<Document>, tools?: ITriggerTools) => void;
+    dropIndexes?: (change: ChangeStreamDocument<Document>, tools?: ITriggerTools) => void;
+    shardCollection?: (change: ChangeStreamDocument<Document>, tools?: ITriggerTools) => void;
+    reshardCollection?: (change: ChangeStreamDocument<Document>, tools?: ITriggerTools) => void;
+    refineCollectionShardKey?: (change: ChangeStreamDocument<Document>, tools?: ITriggerTools) => void;
 }
